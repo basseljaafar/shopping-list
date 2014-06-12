b051adaa708c8743597a75d1133ba8cccae78d5b
basseljaafar <basseljaafar@gmail.com>
2014-06-12 17:07:18 -0500

    Trash can effect

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ $(document).ready(function(){
 
 	
 
-	$(".but").click(function(){
+	$(".but").mousedown(function(){
 	
 	if ($(".text").val() != "")
 		{
+		var fin = "global";
 		var elt = $(".text").val();
 		var fin = $("<li></li>");
 		var num = $("<p></p>");
 		var image = $("<img src= 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTdZJwFeAlT0bzshZ7RPDKeylolDZjvnijQ2khqNE93cldf6eYO'/>");
+		
 		$(num).text(elt);
 		$(num).addClass("element");
 		$(fin).append(num).append(image);
@@ -42,5 +44,25 @@ $(document).ready(function(){
 	}
 	
 	});
+	
+	$("ul").on("mouseenter", "li", function(){
+	$(this).find("img").addClass("display");
+	});
+	
+	$("ul").on("mouseleave", "li", function(){
+	$(this).find("img").removeClass("display");
+	});
+	
+	$("ul").on("mouseenter", "img", function(){
+	$(this).addClass("op");
+	});
+	
+	$("ul").on("mouseleave", "img", function(){
+	$(this).removeClass("op");
+	})
+	
+	//.mouseup(function(){
+	//$(this).parent().slideUp();
+	//});
 	
 });
